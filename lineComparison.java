d175d09ace4c3cb892a9eeb39f0d9b317cf24a5d
MindFaktur <69393019+MindFaktur@users.noreply.github.com>
2021-12-16 11:03:07 +0530

    Update lineComparison.java

--- lineComparison.java
+++ lineComparison.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class lineComparison{
 	public static void main(String[] args){
-		System.out.println("Welcome to Line Comparison computation program");
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter x1 co-ordinate");
+		int valx1 = obj.nextInt();
+		System.out.println("Enter y1 co-ordinate");
+		int valy1 = obj.nextInt();
+		System.out.println("Enter x2 co-ordinate");
+		int valx2 = obj.nextInt();
+		System.out.println("Enter y2 co-ordinate");
+		int valy2 = obj.nextInt();
+		System.out.println("line length is " + (lineLength(valx1,valy1,valx2,valy2)));
+	}
+
+	public static float lineLength(int x1, int y1, int x2, int y2){
+		return (float) Math.sqrt( (Math.pow((x2-x1),2)) + (Math.pow((y2-y1),2)));
+
 	}
 }
